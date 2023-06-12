d1ae4f318e072f21fa3900e329827627e8463de3
Harish <dev.harish3124@gmail.com>
2023-06-13 00:03:43 +0530

    Edit Readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+## Work In Progress!
+---
 # CC-Beats
+Play Copyright free NCS Release songs in OBS as Browser Source
+
+## Features
++ Automatically cycle through random songs
++ Includes Attribution Text
++ Includes /random route for use in other application (returns src url and attribution text as json)
+
+## Routes
+### /banner
+Returns an banner with attribution text and autoplays the song
+
+### /random (For use in other applications)
+Returns source url and attribution text as Json
+
--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def init():
     # library will be a dict of index:(url, attribution_text)
     library = {}
@@ -12,17 +13,17 @@ def init():
     page_no = 1
     while True:
         page = requests.get(base_url.format(page_no))
-        soup = BeautifulSoup(page.text, 'html.parser')
+        soup = BeautifulSoup(page.text, "html.parser")
 
         try:
-            table = soup.find_all('tbody')[1]
-            for song in table.find_all('tr'):
-                song_details = song.find_all('td')[3]
-                name = song_details.find('p').text
-                artist = song_details.find('span').text
-                href = song_details.find('a').get('href')
+            table = soup.find_all("tbody")[1]
+            for song in table.find_all("tr"):
+                song_details = song.find_all("td")[3]
+                name = song_details.find("p").text
+                artist = song_details.find("span").text
+                href = song_details.find("a").get("href")
 
-                url = song.find('td').a.get('data-url')
+                url = song.find("td").a.get("data-url")
 
                 attribution_text = base_attr_text.format(name, artist, href, href)
                 library[index] = (url, attribution_text)
